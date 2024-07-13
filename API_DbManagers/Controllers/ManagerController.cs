0c641f10204813e265eb4ada4cabad821e301114
EngelReyes23 <EngelReyes23@Gmail.com>
2024-07-13 11:36:00 -0600

    Update ManagerController and configs
    - Updated ManagerController.cs to enhance API functionality, including changing return types to ActionResult for flexible responses, implementing CRUD operations with Entity Framework, and handling exceptions more gracefully.
    - Modified launchSettings.json to set the default launch URL to the api/Manager endpoint, improving the initial navigation experience.
    - Added a new connection string in appsettings.json for the DbManagers database, facilitating database connectivity and operations.

--- API_DbManagers/Controllers/ManagerController.cs
+++ API_DbManagers/Controllers/ManagerController.cs
@@ -1,5 +1,7 @@
 ﻿using API_DbManagers.Context;
+using API_DbManagers.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -20,34 +22,97 @@ namespace API_DbManagers.Controllers
 
         // GET: api/<ManagerController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public ActionResult Get()
         {
-            return new string[] { "value1", "value2" };
+            try
+            {
+                var managers = _context.Db_Manager.ToList();
+                return Ok(managers);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         // GET api/<ManagerController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        [HttpGet("{id}", Name = "GetManager")]
+        public ActionResult Get(int id)
         {
-            return "value";
+            try
+            {
+                var manager = _context.Db_Manager.FirstOrDefault(p => p.Id == id);
+                return Ok(manager);
+                
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
         }
 
         // POST api/<ManagerController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public ActionResult Post([FromBody] Db_Manager manager)
         {
+            try
+            {
+                _context.Db_Manager.Add(manager);
+                _context.SaveChanges();
+                return CreatedAtRoute("GetManager", new { id = manager.Id }, manager);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         // PUT api/<ManagerController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public ActionResult Put(int id, [FromBody] Db_Manager manager)
         {
+            try
+            {
+                if (manager.Id == id)
+                {
+                    _context.Entry(manager).State = EntityState.Modified;
+                    _context.SaveChanges();
+                    return CreatedAtRoute("GetManager", new { id = manager.Id }, manager);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         // DELETE api/<ManagerController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public ActionResult Delete(int id)
         {
+            try
+            {
+                var manager = _context.Db_Manager.FirstOrDefault(p => p.Id == id);
+                if (manager != null)
+                {
+                    _context.Db_Manager.Remove(manager);
+                    _context.SaveChanges();
+                    return Ok(id);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
