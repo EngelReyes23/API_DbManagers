5a6821c9e168cbb2509cbdda5d44a20a2288653c
EngelReyes23 <EngelReyes23@Gmail.com>
2024-07-13 10:24:17 -0600

    Add project files.

---
+++ API_DbManagers/Controllers/ManagerController.cs
@@ -0,0 +1,53 @@
+﻿using API_DbManagers.Context;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API_DbManagers.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ManagerController : ControllerBase
+    {
+
+        // import context
+        private readonly AppDbContext _context;
+
+        public ManagerController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<ManagerController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ManagerController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ManagerController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ManagerController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ManagerController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
